94a43ab384dd1a01d7863e3bfc3fc63a75e0e63a
baby7305 <songhaibei7305@163.com>
2018-03-21 18:25:30 +0800

    支持kotlin、Java混合编程

---
+++ src/main/java/JavaHello.java
@@ -0,0 +1,12 @@
+public class JavaHello {
+  public static String JavaHelloString = "Hello from Java!";
+
+  public static String getHelloStringFromKotlin() {
+    return KotlinHelloKt.getKotlinHelloString();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(getHelloStringFromKotlin());
+    System.out.println(KotlinHelloKt.getHelloStringFromJava());
+  }
+}
---
+++ src/main/kotlin/KotlinHello.kt
@@ -0,0 +1,5 @@
+val KotlinHelloString : String = "Hello from Kotlin!"
+
+fun getHelloStringFromJava() : String {
+    return JavaHello.JavaHelloString!!;
+}
